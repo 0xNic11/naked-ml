629764d7221eb9a7f362ab7451643adea3478eee
Abdullah Ashraf <abdallahashraf5501@iloud.com>
2026-02-27 02:11:54 +0200

    Add docstrings to KNN methods for improved documentation

--- knn/knn.py
+++ knn/knn.py
@@ -2,22 +2,38 @@ import numpy as np
 from collections import Counter
 
 def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum(x1 - x2) ** 2)
+    """
+    Compute the Euclidean distance between two vectors.
+    """
+    return np.sqrt(np.sum((x1 - x2) ** 2))
 
 class KNN:
+    """
+    K-Nearest Neighbors classifier.
+    """
     
     def __init__(self, k=3):
         self.k = k
         
+        
     def fit(self, X, y):
+        """
+        Fit the KNN model to the training data.
+        """
         self.X_train = X
         self.y_train = y
     
     def predict(self, X):
+        """
+        Predict the class labels for the given test data.
+        """
         predicted_labels = [self._predict(x) for x in X]
         return np.array(predicted_labels)
     
     def _predict(self, x):
+        """ 
+        Predict the class label for a single test sample.
+        """
         # compute the distances
         distances = [euclidean_distance(x, x_train) for x_train in self.X_train] 
         
@@ -30,4 +46,10 @@ class KNN:
         return most_common[0][0]
     
     def accuracy(self, y_pred, y_test):
+        """
+        Calculate the accuracy of the predictions.
+        """
         return np.sum(y_pred == y_test) / len(y_test)
+    
+    
+    
